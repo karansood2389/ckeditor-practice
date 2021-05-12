e3254ac1f5c9ebbf2c35c0ed30e894cfbe6d9f7c
karansood2389 <karansood2march@gmail.com>
2021-05-12 12:03:31 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+// import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import * as DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ckeditor-practice';
+  title = "ckeditor-practice";
+  public editor = DecoupledEditor;
+  config = {
+    uiColor: '#ffffff',
+    toolbarGroups: [{ name: 'clipboard', groups: ['clipboard', 'undo'] },
+    { name: 'editing', groups: ['find', 'selection', 'spellchecker'] },
+    { name: 'links' }, { name: 'insert' },
+    { name: 'document', groups: ['mode', 'document', 'doctools'] },
+    { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+    { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align'] },
+    { name: 'styles' },
+    { name: 'colors' }],
+    skin: 'kama',
+    resize_enabled: false,
+    removePlugins: 'elementspath,save,magicline',
+    extraPlugins: 'divarea,smiley,justify,indentblock,colordialog',
+    colorButton_foreStyle: {
+       element: 'font',
+       attributes: { 'color': '#(color)' }
+    },
+    height: 188,
+    removeDialogTabs: 'image:advanced;link:advanced',
+    removeButtons: 'Subscript,Superscript,Anchor,Source,Table',
+    format_tags: 'p;h1;h2;h3;pre;div'
+ }
+
+  public onReady(editor) {
+    editor.ui
+      .getEditableElement()
+      .parentElement.insertBefore(
+        editor.ui.view.toolbar.element,
+        editor.ui.getEditableElement()
+      );
+  }
 }
